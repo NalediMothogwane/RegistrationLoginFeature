9fd8b7f96d02d1da4146753e9df63009b8b5b9fe
NALEDI <NALEDI@LAPTOP-FD6J7H1U>
2026-05-25 21:21:20 +0200

    imported my java.util.Scanner;

--- src/main/java/com/mycompany/registrationloginfeature/RegistrationLoginFeature.java
+++ src/main/java/com/mycompany/registrationloginfeature/RegistrationLoginFeature.java
@@ -15,13 +15,13 @@ public class RegistrationLoginFeature {//Start of RegistrationLoginFeature
         Scanner sc = new Scanner(System.in);
 
         // Collect registration details
-        System.out.print("Enter username: "); 
+        System.out.print("Enter username: ");
         String regUsername = sc.nextLine();
 
         System.out.print("Enter first name: ");
         String firstName = sc.nextLine();
 
-        System.out.print("Enter last name: ");
+        System.out.print("Enter last name: "); 
         
         String lastName = sc.nextLine();
 
